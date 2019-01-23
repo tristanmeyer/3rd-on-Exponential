8e7fddaa48e49af2e562e57638756cd911e4dadf
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2019-01-23 18:00:49 -0500

    Updated from Brython Server: 1/23/2019 6:00:49 PM

--- 3rd-on-Exponential.py
+++ 3rd-on-Exponential.py
@@ -1,4 +1,11 @@
 #Tristan Meyer
 #Third Point on Exponential Graph
 
-x1 = int(input("Enter x1: ")
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+x3 = int(input("Enter x3: "))
+sx3 = str(x3)
+equation = y1*(y2/y1)**((x3-x1)/(x2-x1))
+print("f("+sx3+") =",equation)
